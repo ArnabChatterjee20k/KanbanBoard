9f48cd4601ed7678a03cbf0f58693cd6ee6a3fb6
arnab <arnabchatterjee.ac.2@gmail.com>
2023-06-10 14:04:54 +0530

    Added condition for dragging

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
@@ -7,32 +7,46 @@ const Container = styled.div`
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
-  background-color: ${props=>props.isDragging?"lightgreen":"white"};
-  display:flex
+  background-color: ${(props) => (props.isDragging ? "lightgreen" : "white")};
+  display: flex;
 `;
 
 const DragHandle = styled.div`
-  width:20px;
-  height:20px;
-  background-color:orange;
-  border-radius:4px;
-  margin-right:8px;
-`
+  width: 20px;
+  height: 20px;
+  background-color: ${({ disabled })=>disabled?"black":"orange"};
+  border-radius: 4px;
+  margin-right: 8px;
+`;
+
+const DisableHandle = styled.input`
+  margin-left: 8px;
+`;
+
+const Content = styled.span`
+  text-decoration: ${({ disabled }) => (disabled ? "line-through" : "none")};
+  color: ${({ disabled }) => (disabled ? "gray" : "black")};
+`;
 export default function Task({ task, index }) {
+  const [disabled, setDisabled] = useState(false);
   return (
-    <Draggable draggableId={task.id} index={index}>
-      {(provided,snapshot) => {
-        const isDragging = snapshot.isDragging
+    <Draggable draggableId={task.id} index={index} isDragDisabled={disabled}>
+      {(provided, snapshot) => {
+        const isDragging = snapshot.isDragging;
         return (
           <Container
             ref={provided.innerRef}
             {...provided.draggableProps}
             // {...provided.dragHandleProps}
-            isDragging = {isDragging}
+            isDragging={isDragging}
           >
             {/* the handle for dragging */}
-            <DragHandle {...provided.dragHandleProps}/>
-            {task.content}
+            <DragHandle {...provided.dragHandleProps} disabled={disabled}/>
+            <Content disabled={disabled}>{task.content}</Content>
+            <DisableHandle
+              type="checkbox"
+              onChange={() => setDisabled((prev) => !prev)}
+            />
           </Container>
         );
       }}

c48436e7d296f4a713e7bcbc12b99b9e250b4735
arnab <arnabchatterjee.ac.2@gmail.com>
2023-06-15 16:23:44 +0530

    Added Basic Layout

---
+++ src/layout/Navigation.jsx
@@ -0,0 +1,23 @@
+import { Layout, theme } from "antd";
+import React from "react";
+import HeadNav from "./HeadNav";
+import Sidebar from "./Sidebar";
+import { Content } from "antd/es/layout/layout";
+
+const Navigation = ({ children }) => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  return (
+    <Layout className="min-h-screen">
+      <HeadNav />
+      <Content>
+        <Layout>
+          <Sidebar />
+          <Layout className="flex flex-row justify-center">{children}</Layout>
+        </Layout>s
+      </Content>
+    </Layout>
+  );
+};
+export default Navigation;

315dcc0d4918a54ba68dd5d03f8bcb3b8f5b3d0f
arnab <arnabchatterjee.ac.2@gmail.com>
2023-06-29 12:45:54 +0530

    feat -- added go back option in subtasks

--- src/pages/Task/components/BackButton.jsx
+++ src/pages/Task/components/BackButton.jsx
@@ -1,9 +1,14 @@
 import { ArrowLeftOutlined } from "@ant-design/icons";
 import { Button, Layout } from "antd";
+import { useNavigate } from "react-router-dom";
 
 export default function BackButton({ title }) {
+  const nav = useNavigate()
+  function goBack(){
+    nav(-1)
+  }
   return (
-      <Button className="w-fit m-4" icon={<ArrowLeftOutlined />}>
+      <Button className="w-fit m-4" icon={<ArrowLeftOutlined />} onClick={goBack}>
         {title || "Back"}
       </Button>
   );

e342b4477629de4fa7495ac528a636f0588f61a8
arnab <arnabchatterjee.ac.2@gmail.com>
2023-08-03 18:45:51 +0530

    bug -- fixed extra api calls for cols on state change by introducing a cache.

--- src/services/useGetAllCols.jsx
+++ src/services/useGetAllCols.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import api from "../api/API";
 import useFetcher from "../hooks/useFetcher";
 import { useColumnStore } from "../pages/Board/store/columnStore";
@@ -5,14 +6,20 @@ import { useColumnStore } from "../pages/Board/store/columnStore";
 export default function useGetAllCols(board_id, user_id) {
   const setCols = useColumnStore((state) => state.setCols);
   const cols = useColumnStore((state) => state.columns);
+  const cache = useRef(new Map());
+  const key = `${user_id}/${board_id}`;
   async function queryFunc() {
-    if (Object.keys(cols).length === 0) {
+    if (!cache.current.has(key)) {
+      cache.current.set(key, "fetched");
       const columns = await api.fetchAllColumns(user_id, board_id);
-      console.log("🚀 ~ file: useGetAllCols.jsx:11 ~ queryFunc ~ columns:", columns)
+      console.log(
+        "🚀 ~ file: useGetAllCols.jsx:11 ~ queryFunc ~ columns:",
+        columns
+      );
       setCols(columns);
       return columns;
     }
-    return cols
+    return cols;
   }
   return useFetcher(queryFunc, [user_id, board_id]);
 }
